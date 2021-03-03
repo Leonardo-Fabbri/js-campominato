4c69bb231355b1a0d8a49d8868fc7d62de1bd419
Leonardo <leonardofabbri125@gmail.com>
2021-03-03 16:18:37 +0100

    passaggio

--- script.js
+++ script.js
@@ -1,51 +1,65 @@
-var array = [];
-while(array.length < 16){
-    var numeriRandom= Math.floor(Math.random() * 100) + 1; /*restituisce un numero da 0 100*/
-    if(array.indexOf(numeriRandom)== -1) {
-    array.push(numeriRandom);
+var arrayBombe=[];
+var arrayNumeriUtente=[];
+var numBombe=16; /*numero di bombe*/
+var max= 100; /* totale numeri "caselle" livello 0*/
+var mix= 80; /*totale numeri "caselle" livello 1*/
+var mox=50; /*totale numeri "caselle" livello 2*/
+var partitapersa= false;
+
+
+while (arrayBombe.length < numBombe ) { 
+   var numeroGenerato=getRandomNumber(1,100);
+    if (!isInArray(arrayBombe, numeroGenerato)){
+       arrayBombe.push(numeroGenerato);
+       /* guarda riga 63*/
     }
-/*"se il numero non esiste nel array (-1) allora push in array"*/
 }
-console.log(array);
+console.log(arrayBombe.length); /* mostrami nella console i numeri random delle bombe */
 
-var i=0;
-var NumeroUtente;
 
-  
-for (i=1; i<16; i++) {
-    NumeroUtente=prompt("scrivi numero");
-    console.log(NumeroUtente);
- }
+/*cicla fino a 84 volte e fino a che il numero inserito dal giocatore è diverso dal array random:*/
+ while ((arrayNumeriUtente.length < (max-numBombe)) && (arrayBombe, arrayNumeriUtente)== false) { 
+     /*se il numero inserito dall'utente è = a un numero dell'array random sei morto*/
+     if (isInArray(arrayBombe, numeroUtente)){
+      alert("hai perso");
+    }
 
-// while (i<16 || NumeroUtente==numeriRandom) {
-//     NumeroUtente=prompt("scrivi numero");
-//     console.log (NumeroUtente);
-//     if (NumeroUtente==numeriRandom) {
-//         true;
-//         console.log ("hai vinto");
-//     }else {
-//         false;
-       
 
-//     }
+}
 
-// }
+
+function getRandomNumber (min, max) {
+    return Math.floor(Math.random()* (max-min+1)+min);
+}
 
 
 
+function isInArray(array, value) {
+    for (var i=0; i < array.length;i++) {
+        if (value==array[i]) { /*accedi al singolo elemento[i]*/
+            trovato=true;
+            return trovato;  /* se trovo dentro al cilco è vero altrimenti torna ad essere falso*/
+        }
+    }
+    /* se è falsa allora mettilo nella lista delle bombe = arrayBombe)*/
+  return false; /*oppure undefind*/
 
-// while (i<1) {
-//     NumeroUtente=prompt("scrivi numero");
-//     console.log(NumeroUtente);
+}
 
-//     // // if (NumeroUtente==Math.floor) {
-//     //     true;
-//     //     NumeroUtente=console.log ("hai vinto");
 
-//     // } else {
-//     //     false;
 
-//     // }
-// }
 
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
